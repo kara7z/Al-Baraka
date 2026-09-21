03b2283822f43f78588ad6ed13dfe4e977e0fbd4
kara7z <kara.ossama12@gmail.com>
2026-09-21 12:36:05 +0000

    Override with clean Maven albaraka project

---
+++ src/main/java/com/solubank/Main.java
@@ -0,0 +1,12 @@
+package com.solubank;
+
+import com.solubank.Models.User;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello from com.solubank.Main!");
+    System.out.println("Hello world of programming");
+    System.out.println(User.showId());
+  }
+}
---
+++ src/main/java/com/solubank/Models/User.java
@@ -0,0 +1,9 @@
+package com.solubank.Models;
+public class User {
+
+  static int id = 1;
+  public static int showId(){
+
+    return id;
+  }
+}
